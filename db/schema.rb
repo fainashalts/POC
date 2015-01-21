8a027223118668223c7e6d59f2950373e0d4d2c0
fsp712 <fainashalts@gmail.com>
2015-01-20 21:16:57 -0800

    added password_confirm to UsersController

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121034648) do
+ActiveRecord::Schema.define(version: 20150121045148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150121034648) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",      default: false
   end
 
 end
