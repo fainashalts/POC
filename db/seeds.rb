3946902928f8f339a8029225d7205d0dc4cb0de6
fsp712 <fainashalts@gmail.com>
2015-01-31 13:59:48 -0800

    added seeds for topics and subtopics

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,54 @@ Subtopic.destroy_all
 
 
 	topics = Topic.create([
-		{name: "Javascript"},
-		{name: "Ruby"},
-		{name: "HTML"},
-		{name: "CSS"}
+		{name: "Programming Languages"},
+		{name: "Testing"}, 
+		{name: "Version Control"}, 
+		{name: "Database Design"}, 
+		{name: "Data Science"}, 
+		{name: "Mobile Development"}, 
+		{name: "Web Design"}, 
+		{name: "APIs"}, 
+		{name: "Command Line"}, 
+		{name: "Math for Programmers"}
 	])
 
-  	subtopic = Subtopic.create([
-	    {name: "EmberJS", topic_id: 1},
-	    {name: "AngularJS", topic_id: 1}, 
-	    {name: "jQuery", topic_id: 1},
-	    {name: "Rails", topic_id: 2}, 
-	    {name: "Methods", topic_id: 2}, 
-	    {name: "Best Practices", topic_id: 2}, 
-	    {name: "HTML 5", topic_id: 3}, 
-	    {name: "Best Practices", topic_id: 3}, 
-	    {name: "Tags", topic_id: 3}, 
-	    {name: "Bootstrap", topic_id: 4}, 
-	    {name: "Sass", topic_id: 4}, 
-	    {name: "Best Practices", topic_id: 4}
-    ]) 
+	subtopic = Subtopic.create([
+		{name: "Javascript", topic_id: 1}, 
+		{name: "HTML", topic_id: 1}, 
+		{name: "CSS", topic_id: 1}, 
+		{name: "Ruby", topic_id: 1}, 
+		{name: "C", topic_id: 1}, 
+		{name: "C++", topic_id: 1}, 
+		{name: "Python", topic_id: 1}, 
+		{name: "PHP", topic_id: 1}, 
+		{name: "Test-Driven Development", topic_id: 2},
+		{name: "Agile Methods", topic_id: 2},
+		{name: "Testing Basics", topic_id: 2},
+		{name: "Git", topic_id: 3},
+		{name: "GitHub", topic_id: 3},
+		{name: "Continuous Integration", topic_id: 3}, 
+		{name: "Version Control Basics", topic_id: 3}, 
+		{name: "SQL", topic_id: 4},
+		{name: "NoSQL", topic_id: 4},
+		{name: "Database Basics", topic_id: 4},
+		{name: "Machine Learning", topic_id: 5},
+		{name: "Modeling Techniques", topic_id: 5},
+		{name: "Data Science Basics", topic_id: 5},
+		{name: "iOS", topic_id: 6},
+		{name: "Android", topic_id: 6},
+		{name: "Windows Mobile", topic_id: 6},
+		{name: "Mobile Development Basics", topic_id: 6},
+		{name: "UX Design", topic_id: 7},
+		{name: "Wireframing", topic_id: 7},
+		{name: "Web Design Basics", topic_id: 7},
+		{name: "Building an API", topic_id: 8},
+		{name: "Consuming an API", topic_id: 8},
+		{name: "API Basics", topic_id: 8},
+		{name: "Command Line Basics", topic_id: 9},
+		{name: "Command Line Tips and Tricks", topic_id: 9},
+		{name: "Algorithms", topic_id: 10},
+		{name: "Discrete Mathematics", topic_id: 10},
+		{name: "Linear Algebra", topic_id: 10}
+  ]) 
 
